cff18780108b92ee6232780541491219d1479ef3
tofayel <tofayeltuhin143@gmail.com>
2025-05-03 15:31:55 +0600

    add ultimate model secxtion

--- src/pages/main/Home/index.jsx
+++ src/pages/main/Home/index.jsx
@@ -1,5 +1,6 @@
 import Banner from "@/components/main/home/banner";
 import HowItWork from "@/components/main/home/howitwork";
+import UltimateModel from "@/components/main/home/unlimitedmodel";
 
 const Home = () => {
   return (
@@ -7,6 +8,7 @@ const Home = () => {
       <div>
         <Banner />
         <HowItWork />
+        <UltimateModel />
       </div>
     </>
   );
