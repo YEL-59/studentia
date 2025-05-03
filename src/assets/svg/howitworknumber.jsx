bcab297b0e96d3180961c22757c8d877c36b4153
tofayel <tofayeltuhin143@gmail.com>
2025-05-03 11:59:01 +0600

    add a marque effect

---
+++ src/assets/svg/howitworknumber.jsx
@@ -0,0 +1,97 @@
+const Howitworknumber = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="80"
+      height="81"
+      viewBox="0 0 80 81"
+      fill="none"
+    >
+      <mask
+        id="mask0_680_2838"
+        // style="mask-type:alpha"
+        maskUnits="userSpaceOnUse"
+        x="0"
+        y="0"
+        width="80"
+        height="81"
+      >
+        <circle
+          cx="40"
+          cy="40.7881"
+          r="39.7163"
+          fill="black"
+          stroke="black"
+          stroke-width="0.567376"
+        />
+      </mask>
+      <g mask="url(#mask0_680_2838)">
+        <path
+          d="M33.3242 -6.60352C39.0025 -1.09727 42.4683 6.30704 43.0742 14.2012L43.125 14.9727C43.5472 22.9529 41.0176 30.7984 36.04 37.0059L35.5508 37.6016C30.4157 43.7017 23.2237 47.6695 15.3672 48.7578L14.6045 48.8545C6.49815 49.78 -1.65286 47.5759 -8.2041 42.6934V24.2764C-8.2082 22.1695 -7.69461 20.0965 -6.71094 18.2412L-6.50781 17.873C-5.39227 15.9263 -3.7865 14.3089 -1.85156 13.1855L-1.85059 13.1865L30.6836 -5.46484L30.6846 -5.46582C31.5173 -5.94759 32.4036 -6.32879 33.3242 -6.60352Z"
+          fill="url(#paint0_linear_680_2838)"
+          stroke="black"
+          stroke-width="0.567376"
+        />
+        <path
+          d="M66.7812 66.1582C67.4059 66.1606 68.0245 66.2742 68.6084 66.4922L68.8564 66.5918C69.4435 66.8455 69.9782 67.2042 70.4365 67.6484C68.5318 70.5997 66.2902 73.3189 63.7559 75.751C62.949 75.1798 62.3187 74.3933 61.9365 73.4814L61.8574 73.2832C61.4851 72.2832 61.4254 71.1953 61.6826 70.1631L61.7383 69.958C62.0377 68.9339 62.6396 68.0261 63.4629 67.3525L63.6299 67.2207C64.5384 66.5365 65.6439 66.1638 66.7812 66.1582Z"
+          fill="white"
+          stroke="black"
+          stroke-width="0.567376"
+        />
+      </g>
+      <foreignObject x="-44.1305" y="-43.3424" width="168.261" height="168.261">
+        <div
+          xmlns="http://www.w3.org/1999/xhtml"
+          style={{
+            backdropFilter: "blur(22.07px)",
+            clipPath: "url(#bgblur_0_680_2812_clip_path)",
+            height: "100%",
+            width: "100%",
+          }}
+        ></div>
+      </foreignObject>
+      <circle
+        data-figma-bg-blur-radius="44.1305"
+        cx="40"
+        cy="40.7881"
+        r="39.4326"
+        fill="black"
+        fill-opacity="0.05"
+        stroke="url(#paint1_linear_680_2838)"
+        stroke-width="1.13475"
+      />
+      <defs>
+        <clipPath
+          id="bgblur_0_680_2838_clip_path"
+          transform="translate(44.1305 43.3424)"
+        >
+          <circle cx="40" cy="40.7881" r="39.4326" />
+        </clipPath>
+        <linearGradient
+          id="paint0_linear_680_2838"
+          x1="17.4827"
+          y1="-6.92578"
+          x2="17.4827"
+          y2="49.3447"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stop-color="#7A43A4" />
+          <stop offset="1" stop-color="#6049BC" />
+        </linearGradient>
+        <linearGradient
+          id="paint1_linear_680_2838"
+          x1="40"
+          y1="0.788086"
+          x2="40"
+          y2="80.7881"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stop-color="#7A43A4" />
+          <stop offset="1" stop-color="#6049BC" />
+        </linearGradient>
+      </defs>
+    </svg>
+  );
+};
+
+export default Howitworknumber;
---
+++ src/components/main/home/logomarque/index.jsx
@@ -0,0 +1,42 @@
+import Marquee from "react-fast-marquee";
+import logo1 from "../../../../assets/Logo1.png";
+import logo2 from "../../../../assets/Logo2.png";
+import logo3 from "../../../../assets/Logo3.png";
+import logo4 from "../../../../assets/Logo4.png";
+import logo5 from "../../../../assets/Logo5.png";
+import logo6 from "../../../../assets/Logo6.png";
+
+const logos = [logo1, logo2, logo3, logo4, logo5, logo6, logo3, logo4];
+
+const LogoMarquee = () => {
+  return (
+    <div className="w-full bg-[#070622] py-8">
+      <Marquee
+        speed={40}
+        gradient={false}
+        pauseOnClick={true}
+        direction="left"
+        loop={0}
+        delay={0}
+        play={true}
+        pauseOnHover={true}
+        className="flex items-center"
+      >
+        {logos.map((src, index) => (
+          <div
+            key={index}
+            className="flex w-[205.429px] h-[85.595px] flex-col justify-center items-center gap-[8.56px] shrink-0 px-4"
+          >
+            <img
+              src={src}
+              alt={`Logo ${index + 1}`}
+              className="max-h-full max-w-full object-contain filter grayscale brightness-[0.5] hover:filter-none transition-all duration-300"
+            />
+          </div>
+        ))}
+      </Marquee>
+    </div>
+  );
+};
+
+export default LogoMarquee;
