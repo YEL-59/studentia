3cdf5fa9f56c3bfdbfb5817eebdbe78dd1270080
tofayel <tofayeltuhin143@gmail.com>
2025-05-06 16:48:09 +0600

    add route

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,30 +1,10 @@
+import Articelgenerate from "@/components/dashboard/articelgenerate";
 import AiAssistantLayout from "@/layouts/ai-assistant-layout";
 import AiChatLayout from "@/layouts/ai-chat-layout";
 import DashboardLayout from "@/layouts/dashboard-layout";
 import MainLayout from "@/layouts/MainLayout";
 import AiAssistant from "@/pages/ai-assistant/ai-assistant";
 import AiChat from "@/pages/ai-assistant/chat-with-ai-assistant/ai-chat";
-import Account from "@/pages/dashboard/account";
-import AiTutors from "@/pages/dashboard/ai-tutors";
-import Articles from "@/pages/dashboard/articles";
-import Codes from "@/pages/dashboard/codes";
-import Dashboard from "@/pages/dashboard/dashboard";
-import Documents from "@/pages/dashboard/documents";
-import Humanizer from "@/pages/dashboard/humanizer";
-import ImageGenerator from "@/pages/dashboard/image-generator";
-import RevisionTools from "@/pages/dashboard/revision-tools";
-import SubscriptionPanel from "@/pages/dashboard/subscription-panel";
-import Workbooks from "@/pages/dashboard/workbooks";
-import WritingTools from "@/pages/dashboard/writing-tools";
-import Signin from "@/pages/main/auth/signin";
-import Signup from "@/pages/main/auth/signup";
-import Home from "@/pages/main/Home";
-import { createBrowserRouter } from "react-router";
-import Articelgenerate from "@/components/dashboard/articelgenerate";
-import AiAssistantLayout from "@/layouts/ai-assistant-layout";
-import DashboardLayout from "@/layouts/dashboard-layout";
-import MainLayout from "@/layouts/MainLayout";
-import AiAssistant from "@/pages/ai-assistant/ai-assistant";
 import Account from "@/pages/dashboard/account";
 import AiTutors from "@/pages/dashboard/ai-tutors";
 import Aicode from "@/pages/dashboard/aicode";
@@ -78,12 +58,10 @@ export const router = createBrowserRouter([
         element: <AiTutors />,
       },
       {
-        path: "documents",
         path: "/dashboard/documents",
         element: <Documents />,
       },
       {
-        path: "codes",
         path: "/dashboard/documents/codes",
         element: <Codes />,
       },
@@ -112,7 +90,7 @@ export const router = createBrowserRouter([
   },
   {
     path: "/ai-assistant",
-    path: "/ai-assistant",
+
     element: <AiAssistantLayout />,
     children: [
       {
@@ -125,9 +103,7 @@ export const router = createBrowserRouter([
       },
     ],
   },
-  {
-    path: "sign-up",
-  },
+
   {
     path: "/ai-chat/:id",
     element: <AiChatLayout />,
